400e105da26c3a943a67e55ecca988af345b0056
Aletziz <galexito856@gmail.com>
2025-03-28 19:20:17 -0400

    Add files via upload

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,3 @@
-const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://alex-store-backend.vercel.app/routes"
-    : "http://localhost:5000/routes";
+const API_URL = "https://alex-store-backend.vercel.app/api";
 
 export default API_URL;
